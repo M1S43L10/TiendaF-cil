ecf78fe8ed9031c045b2079fa837c2ed6dc1224a
M1S43L <misaelr893@gmail.com>
2025-08-04 23:52:06 -0300

    CreaTuLanding1+Ramirez

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,13 @@
+
+const CartWidget = () => {
+  return (
+    <button className="btn btn-outline-light position-relative">
+      🛒
+      <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+        3
+      </span>
+    </button>
+  );
+};
+
+export default CartWidget;
